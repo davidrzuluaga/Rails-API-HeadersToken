59af78a586f06dc3b99d780646e31fbc3c6235c6
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-24 17:05:18 -0500

    idem.v1

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,17 +1,21 @@
 class Api::V1::PinsController < ApplicationController
-  protect_from_forgery with: :null_session
-  before_action :basic_auth
+  skip_before_action :verify_authenticity_token
+  before_action :validate_user_and_token
 
   def index
     render json: Pin.all.order('created_at DESC')
   end
 
   def create
-    pin = Pin.new(pin_params)
-    if pin.save
-      render json: pin, status: 201
+    if @valid_keys
+      pin = Pin.new(pin_params)
+      if pin.save
+        render json: pin, status: 201
+      else
+        render json: { errors: pin.errors }, status: 422
+      end
     else
-      render json: { errors: pin.errors }, status: 422
+      head 401
     end
   end
 
@@ -20,12 +24,16 @@ class Api::V1::PinsController < ApplicationController
       params.require(:pin).permit(:title, :image_url)
     end
 
-    def basic_auth 
-      authenticate_or_request_with_http_basic do |user, token|
-        user = User.find_by_email(user)
-        if user.api_token == token
-          sign_in user
-        end
+    def validate_user_and_token
+      @valid_keys = false
+      if request.headers["X-User-Email"] && request.headers["X-Api-Token"]
+        @user = User.find_by(email: request.headers["X-User-Email"])  
+        token_is_valid?(@user) if @user
       end
+      puts @valid_keys
+    end
+
+    def token_is_valid?(user)
+      @valid_keys = true if request.headers["X-Api-Token"] == user.api_token
     end
 end
