ea19361b961dfa295c5fa470c951feee59fd7b3c
DavidRZuluaga <git@davidrzuluaga.com>
2018-09-24 17:28:37 -0500

    MKR Solut

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,26 +1,17 @@
 class Api::V1::PinsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  before_action :validate_user_and_token
+  before_action :authenticate_api!
 
   def index
-    if @valid_keys
       render json: Pin.all.order('created_at DESC')
-    else
-      head 401
-    end
   end
 
   def create
-    if @valid_keys
       pin = Pin.new(pin_params)
       if pin.save
         render json: pin, status: 201
       else
         render json: { errors: pin.errors }, status: 422
       end
-    else
-      head 401
-    end
   end
 
   private
@@ -28,16 +19,12 @@ class Api::V1::PinsController < ApplicationController
       params.require(:pin).permit(:title, :image_url)
     end
 
-    def validate_user_and_token
-      @valid_keys = false
-      if request.headers["X-User-Email"] && request.headers["X-Api-Token"]
-        @user = User.find_by(email: request.headers["X-User-Email"])  
-        token_is_valid?(@user) if @user
+    def authenticate_api!
+      email = request.headers['HTTP_X_USER_EMAIL']
+      token = request.headers['HTTP_X_API_TOKEN']
+      @current_user = User.where(email: email).take
+      unless @current_user && @current_user.api_token == token
+        head 401
       end
-      puts @valid_keys
-    end
-
-    def token_is_valid?(user)
-      @valid_keys = true if request.headers["X-Api-Token"] == user.api_token
     end
 end
